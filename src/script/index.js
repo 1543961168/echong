85fff0ba1c41851fcfc17b134ed3acadb6f8dcd9
clt <1543961168@qq.com>
2020-06-04 21:10:04 +0800

    1.0.3

--- src/script/index.js
+++ src/script/index.js
@@ -1,34 +1,73 @@
-!function($){
-  // console.log(2)
-  //导入头部组建
-  $(document).ready(function(){
-    $("header").load("header.html")
-    console.log(1)
-  })
-  //导入尾部组建
-  $(document).ready(function(){
-    $("footer").load("footer.html")
-    console.log(1)
-  })
-
-  function erji(){
-    $("#erji").find("li").mouseover(function() {
-      var index = $(this).index();
-      $("#erji").find("li").eq(index).css({
-          "border-top": "1px solid #68af5c",
-          "border-bottom": "1px solid #68af5c",
-      });
-      $("#erji").find("div").eq(index).show().siblings("div").hide();
-    });
-    $("#erji").find("li").mouseleave(function() {
-      var index = $(this).index();
-      $("#erji").find("li").eq(index).css({
-          "border": "none"
-      });
+! function($) {
+    // console.log(2)
+    //导入头部组建
+    $(document).ready(function() {
+            $("header").load("header.html")
+                // console.log(1)
+        })
+        //导入尾部组建
+    $(document).ready(function() {
+        $("footer").load("footer.html")
+            // console.log(1) 
     })
-    $("#erji").mouseleave(function() {
-      $("#erji").find("div").hide();
-   });
-  }
-  erji()
+
+    function erji() {
+        $("#erji").find("li").mouseover(function() {
+            var index = $(this).index();
+            $("#erji").find("li").eq(index).css({
+                "border-top": "1px solid #68af5c",
+                "border-bottom": "1px solid #68af5c",
+            });
+            $("#erji").find("div").eq(index).show().siblings("div").hide();
+        });
+        $("#erji").find("li").mouseleave(function() {
+            var index = $(this).index();
+            $("#erji").find("li").eq(index).css({
+                "border": "none"
+            });
+        })
+        $("#erji").mouseleave(function() {
+            $("#erji").find("div").hide();
+        });
+    }
+    erji()
+
+
+    //渲染数据
+    //  const $boxlist1 = $('#tabbox');
+    //  console.log($boxlist1)
+    //   $.ajax({
+    //     url: 'http://10.31.162.28/echong/php/listdata.php',
+    //     dataType: 'json'
+    //     }).done(function (data) {
+    //         let $strhtml = '<div class="boxlist li3">';
+    //         $strhtml += '<ul class="li3-1">'
+    //         $.each(data, function (index, value) {
+
+    //           $strhtml += `
+    //           <li class="topdong">
+    //             <a href="#">
+    //                 <img class="lazy" data-original="${value.url}" alt="">
+    //                 <span>${value.title}</span>
+    //                 <span>￥${value.price}</span>
+    //             </a>
+    //         </li>
+    //        `;
+
+    //         if(index==7){
+    //          return false
+    //         }
+
+    //   });
+    //   $strhtml += '</ul>'
+    //     $strhtml += '</div>';
+    //     $boxlist1.html($strhtml);
+    // });
+    //图片懒加载
+    //添加懒加载
+    $(function() {
+        $("img.lazy").lazyload({ effect: "fadeIn" });
+    });
+
+
 }(jQuery)
--- src/script/list.js
+++ src/script/list.js
@@ -1,116 +1,163 @@
-var uid = localStorage.getItem("id")
-var username = localStorage.getItem("username")
-var token = localStorage.getItem("token")
-console.log(uid, username, token)
+! function($) {
+    //导入头部组建
+    $(document).ready(function() {
+        $("header").load("header.html")
+    });
+    //导入尾部组建
+    $(document).ready(function() {
+        $("footer").load("footer.html")
+            // console.log(1) 
+    })
+    let array_default = []; //排序前的li数组
+    let array = []; //排序中的数组
+    let prev = null;
+    let next = null;
 
-$.get('http://jx.xuzhixiang.top/ap/api/productlist.php', {
-    uid: uid
-}, function(data) {
-    console.log(data);
-    data = data.data;
-    var arr = data;
-    var str = '';
 
-    for (var i = 0; i < 4; i++) {
-        arr.forEach(function(val) {
-            // console.log(element);
-            str +=
-                "<li uid=\"".concat(val.pid, "\">\n   <a href=\"../详情/index.html?id=")
-                .concat(val.pid, "\"> <img src=\"")
-                .concat(val.pimg, "\" alt=\"\">\n     <div>\n      <h2>")
-                .concat(val.pprice, "</h2>\n                        <h3>")
-                .concat(parseInt(val.pprice / (parseFloat(val.pdesc) / 10)), "</h3> <span>")
-                .concat(val.pdesc, "</span></div>\n                    <p>")
-                .concat(val.pname, "</p>\n                </a>\n                </li>");
+    //1.渲染列表页的数据-默认渲染第一页
+    const $list = $('.list');
+    $.ajax({
+        url: 'http://10.31.162.28/echong/php/listdata.php',
+        dataType: 'json'
+    }).done(function(data) {
+        let $strhtml = '<ul>';
+        $.each(data, function(index, value) {
+            $strhtml += `
+                <li>
+                    <a href="detail.html?sid=${value.sid}" target="_blank">
+                        <img class="lazy" data-original="${value.url}" width="200" height="200"/>
+                        <p>${value.sid}${value.title}</p>
+                        <span class="price">￥${value.price}</span>
+                        <span class="kucun">库存:${value.sailnumber}</span>
+                    </a>
+                </li>
+            `;
         });
-    }
+        $strhtml += '</ul>';
+        $list.html($strhtml);
 
-    $('#list').html(str);
-});
-//添加商品
-$('#test').click(function() {
-    $.post('http://jx.xuzhixiang.top/ap/api/goods/goods-add.php', {
-        pimg: '../img/list1.jpg',
-        pname: '爽口脆',
-        pprice: '239',
-        pdesc: '1.2折',
-        uid: uid
-    }, data => {
-        console.log(data);
+        //添加懒加载
+        $(function() {
+            $("img.lazy").lazyload({ effect: "fadeIn" });
+        });
+
+        array_default = []; //排序前的li数组
+        array = []; //排序中的数组
+        prev = null;
+        next = null;
+        //将页面的li元素加载到两个数组中
+        $('.list li').each(function(index, element) {
+            array[index] = $(this);
+            array_default[index] = $(this);
+        });
+    });
+    //渲染的外部无法获取内部的元素对象，通过事件委托实现。
+
+    //2.分页思路
+    //告知后端当前请求的是第几页数据。将当前的页面页码传递给后端(get和page)
+    $('.page').pagination({
+        pageCount: 3, //总的页数
+        jump: true, //是否开启跳转到指定的页数，布尔值。
+        coping: true, //是否开启首页和尾页，布尔值。
+        prevContent: '上一页',
+        nextContent: '下一页',
+        homePage: '首页',
+        endPage: '尾页',
+        callback: function(api) {
+            console.log(api.getCurrent()); //获取的页码给后端
+            $.ajax({
+                url: 'http://10.31.162.28/echong/php/listdata.php',
+                data: {
+                    page: api.getCurrent()
+                },
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '<ul>';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+                            <a href="detail.html?sid=${value.sid}" target="_blank">
+                                <img src="${value.url}"/>
+                                <p>${value.sid}${value.title}</p>
+                                <span class="price">￥${value.price}</span>
+                                <span class="kucun">库存:${value.sailnumber}</span>
+                            </a>
+                        </li>
+                    `;
+                });
+                $strhtml += '</ul>';
+                $list.html($strhtml);
+
+                array_default = []; //排序前的li数组
+                array = []; //排序中的数组
+                prev = null;
+                next = null;
+
+                //将页面的li元素加载到两个数组中
+                $('.list li').each(function(index, element) {
+                    array[index] = $(this);
+                    array_default[index] = $(this);
+                });
+            })
+        }
+    });
+
+    //3.排序
+
+    $('button').eq(0).on('click', function() {
+        $(this).addClass('qie').siblings('button').removeClass('qie')
+        $.each(array_default, function(index, value) {
+            $('.list ul').append(value);
+        });
+        return;
+    });
+    $('button').eq(1).on('click', function() {
+        $(this).addClass('qie').siblings('button').removeClass('qie')
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                prev = parseFloat(array[j].find('.price').html().substring(1));
+                next = parseFloat(array[j + 1].find('.price').html().substring(1));
+                //通过价格的判断，改变的是li的位置。
+                if (prev > next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        //清空原来的列表，将排序后的数据添加上去。
+        //empty() : 删除匹配的元素集合中所有的子节点。
+        // $('.list ul').empty();//清空原来的列表
+        //这里能够省略empty
+        //append在追加的时候，如果追加的是jquery的元素对象，而jquery元素对象在你追加的元素中存在，直接取出存在的元素，从后面追加。
+        //如果追加的是内容结构，依然和appendChild一样，后面继续追加。
+        $.each(array, function(index, value) {
+            // console.log(value); //n.fn.init [li, context: li]
+            $('.list ul').append(value);
+        });
+    });
+    $('button').eq(2).on('click', function() {
+        $(this).addClass('qie').siblings('button').removeClass('qie')
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                prev = parseFloat(array[j].find('.price').html().substring(1));
+                next = parseFloat(array[j + 1].find('.price').html().substring(1));
+                //通过价格的判断，改变的是li的位置。
+                if (prev < next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        //清空原来的列表，将排序后的数据添加上去。
+        //empty() : 删除匹配的元素集合中所有的子节点。
+        // $('.list ul').empty();//清空原来的列表
+        $.each(array, function(index, value) {
+            // console.log(value);
+            $('.list ul').append(value);
+        });
     })
-    $.post('http://jx.xuzhixiang.top/ap/api/goods/goods-add.php', {
-        pimg: '../img/list2.jpg',
-        pname: '咸鱼',
-        pprice: '339',
-        pdesc: '2.6折',
-        uid: uid
-    }, data => {
-        console.log(data);
-    })
-    $.post('http://jx.xuzhixiang.top/ap/api/goods/goods-add.php', {
-        pimg: '../img/list3.jpg',
-        pname: '第二条咸鱼',
-        pprice: '209',
-        pdesc: '2.7折',
-        uid: uid
-    }, data => {
-        console.log(data);
-    })
-    $.post('http://jx.xuzhixiang.top/ap/api/goods/goods-add.php', {
-            pimg: '../img/list4.jpg',
-            pname: '第三条咸鱼',
-            pprice: '179',
-            pdesc: '2.4折',
-            uid: uid
-        }, data => {
-            console.log(data);
-        })
-        // //获取商品列表
-    $.get('http://jx.xuzhixiang.top/ap/api/productlist.php', {
-        uid: uid
-    }, data => {
-        console.log('刷新后');
-        console.log(data);
-    })
-});
-//  // //删除商品，
-// $.get('http://jx.xuzhixiang.top/ap/api/goods/goods-delete.php', {
-//     pid: '197020',
-//     uid: uid,
-//     token: token
-// }, data => {
-//     console.log(data);
-// })
-// $.get('http://jx.xuzhixiang.top/ap/api/goods/goods-delete.php', {
-//     pid: '197022',
-//     uid: uid,
-//     token: token
-// }, data => {
-//     console.log(data);
-// })
-// $.get('http://jx.xuzhixiang.top/ap/api/goods/goods-delete.php', {
-//     pid: '197017',
-//     uid: uid,
-//     token: token
-// }, data => {
-//     console.log(data);
-// })
-// $.get('http://jx.xuzhixiang.top/ap/api/goods/goods-delete.php', {
-//     pid: '197015',
-//     uid: uid,
-//     token: token
-// }, data => {
-//     console.log(data);
-// })
 
 
-
-/*$.post("http://jx.xuzhixiang.top/ap/api/goods/goods-add.php", {
-        pname: "商品2",
-        pprice: 100,
-        pimg: "imgs/1.jpg",
-        pdesc: "这是一件物美价廉的商品",
-        uid: "32887"
-      }).then(data => {
-        console.log(data);
-      }); */
+}(jQuery);
